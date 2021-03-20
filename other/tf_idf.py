6b71e5309ae331315d866a212a8f01681c8fdb7d
Demetris Kaizer <demetriskaizer@pop-os.localdomain>
2021-03-20 20:27:23 +0200

    update on kNN

--- other/tf_idf.py
+++ other/tf_idf.py
@@ -3,7 +3,7 @@ import re
 
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
-
+from sklearn import metrics
 
 train = pd.read_csv("../dataset/Data_Train.csv", header=0, delimiter=",", quoting=2)
 all_document=''
@@ -43,12 +43,12 @@ for i in range(length):
     clean_train.append( clear_text(train["STORY"][i] ) )
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-print("Creating the tf/idf...\n")
+# print("Creating the tf/idf...\n")
 
 # Initialize the "TfidfVectorizer" object, which is scikitlearn's tf/idf tool.
 tfidf_vectorizer = TfidfVectorizer( max_df=1.0, \
                                     max_features=25000, \
-                                    min_df=0.001, \
+                                    min_df=0.01, \
                                     stop_words=None, \
                                     use_idf=True, \
                                     tokenizer=None, \
@@ -57,24 +57,29 @@ tfidf_vectorizer = TfidfVectorizer( max_df=1.0, \
 tfidf_train_data_features = tfidf_vectorizer.fit_transform(clean_train)
 # Convert the result to nampy array
 tfidf_train_data_features = tfidf_train_data_features.toarray()
-print(tfidf_train_data_features.shape) # (25000, 48)
+print('Features = ', tfidf_train_data_features.shape) # (25000, 48)
 # Take a look at the words in the vocabulary
 tfidf_vocab = tfidf_vectorizer.get_feature_names()
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import model_selection
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(tfidf_train_data_features, train["SECTION"], test_size=0.2, random_state=0)
+from sklearn.metrics import accuracy_score
 
 best_accuracy = 0
 best_n=0
 
 for n in range(1, 20):
+    print('Running KNeighborsClassifier: ', n, '/ 20', end='\r')
     knn = KNeighborsClassifier(n_neighbors=n, n_jobs=-1)
     kfold = model_selection.KFold(n_splits=10)
-    accuracy = model_selection.cross_val_score(knn, X_train, y_train, cv=kfold)
+    accuracy = model_selection.cross_val_score(knn, tfidf_train_data_features, train["SECTION"], cv=kfold)
     if best_accuracy<accuracy.mean():
-        best_accuracy= accuracy.mean()
+        best_accuracy = accuracy.mean()
         best_n=n
 
-print(accuracy.mean(), n)
+knn = KNeighborsClassifier(n_neighbors=best_n, n_jobs=-1)
+model = knn.fit(tfidf_train_data_features, train["SECTION"])
+
+
+print('Best accuracy in training is: ', best_accuracy, 'w/ number of neighbors: ', best_n)
