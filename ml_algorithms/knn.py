003e255d93afb0e2e68e767a16641230c65d832e
erotokritoscy <erotokritos.cy@gmail.com>
2021-03-21 18:09:53 +0200

    tfidf

--- ml_algorithms/knn.py
+++ ml_algorithms/knn.py
@@ -7,7 +7,6 @@ from sklearn.metrics import accuracy_score
 def kfold(x, y, max_n):
     # X_train, X_test, y_train, y_test = train_test_split(tfidf_train_data_features, train["SECTION"], test_size=0.2,
     # random_state=0)
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
     best_accuracy = 0
     best_n = 0
@@ -15,7 +14,7 @@ def kfold(x, y, max_n):
     for n in range(1, max_n):
         knn = KNeighborsClassifier(n_neighbors=n, n_jobs=-1)
         kfold = model_selection.KFold(n_splits=10)
-        accuracy = model_selection.cross_val_score(knn, X_train, y_train, cv=kfold)
+        accuracy = model_selection.cross_val_score(knn, x, y, cv=kfold)
         if best_accuracy < accuracy.mean():
             best_accuracy = accuracy.mean()
             best_n = n
