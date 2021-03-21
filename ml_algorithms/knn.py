cd6fbc7ab6e5f131b1c6758424ab9370bcb863aa
erotokritoscy <erotokritos.cy@gmail.com>
2021-03-21 15:09:23 +0200

    tfidf

--- ml_algorithms/knn.py
+++ ml_algorithms/knn.py
@@ -1,17 +1,24 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import model_selection
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(tfidf_train_data_features, train["SECTION"], test_size=0.2, random_state=0)
+from sklearn.metrics import accuracy_score
 
-best_accuracy = 0
-best_n=0
 
-for n in range(1, 20):
-    knn = KNeighborsClassifier(n_neighbors=n, n_jobs=-1)
-    kfold = model_selection.KFold(n_splits=10)
-    accuracy = model_selection.cross_val_score(knn, X_train, y_train, cv=kfold)
-    if best_accuracy<accuracy.mean():
-        best_accuracy= accuracy.mean()
-        best_n=n
+def kfold(x, y, max_n):
+    # X_train, X_test, y_train, y_test = train_test_split(tfidf_train_data_features, train["SECTION"], test_size=0.2,
+    # random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-print(accuracy.mean(), n)
+    best_accuracy = 0
+    best_n = 0
+
+    for n in range(1, max_n):
+        knn = KNeighborsClassifier(n_neighbors=n, n_jobs=-1)
+        kfold = model_selection.KFold(n_splits=10)
+        accuracy = model_selection.cross_val_score(knn, X_train, y_train, cv=kfold)
+        if best_accuracy < accuracy.mean():
+            best_accuracy = accuracy.mean()
+            best_n = n
+
+    return best_accuracy, best_n
+
--- start.py
+++ start.py
@@ -1,9 +1,14 @@
 import pandas as pd
 from data_processing import tf_idf
-
+from ml_algorithms import knn
 
 train = pd.read_csv("dataset/Data_Train.csv", header=0, delimiter=",", quoting=2)
 all_document = ''
 
-train_data_features, tfidf_vocab = tf_idf.clear_documents(train)
-print(tfidf_vocab)
+train_data_features, tf_idf_vocab = tf_idf.clear_documents(train)
+print(tf_idf_vocab)
+
+best_accuracy, best_n = knn.kfold(train_data_features, train["SECTION"], 5)
+print('best accuracy: ', best_accuracy)
+print('best neighbors: ', best_n)
+
